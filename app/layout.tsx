e8b7796d4aed4d329c9d23bbdaea3a7f43adca80
tonimasc <tonimxc@gmail.com>
2026-02-11 10:44:12 +0100

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 
@@ -11,12 +10,6 @@ const playfairDisplay = Playfair_Display({
   variable: "--font-playfair-display",
   subsets: ["latin"],
 });
-
-export const metadata: Metadata = {
-  title: "Nos casamos y queremos que estés allí",
-  description:
-    "El sábado 6 de junio de 2026, a las 18:00 h, nos casamos en el Palacio de la Margarita, en Collado Villalba (Madrid). Nos haría mucha ilusión celebrarlo contigo.",
-};
 
 export default function RootLayout({
   children,
